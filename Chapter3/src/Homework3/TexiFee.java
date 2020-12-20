a441012df17d78e97f5832ff591a652e243b1570
Chaofan Li <68770750+Clarklevis1995@users.noreply.github.com>
2020-12-20 20:57:15 +0800

    update taxi

--- Chapter3/src/Homework3/TexiFee.java
+++ Chapter3/src/Homework3/TexiFee.java
@@ -14,7 +14,7 @@ public class TexiFee {
 //        int nightRush = minutesInRushHour(new Time(7,0),new Time(17,30),false);
 //        System.out.println(morRush);
 //        System.out.println(nightRush);
-        Time minInNight = inNight(new Time(0,20), new Time(2,50));
+        Time minInNight = inNight(new Time(12, 20), new Time(2, 50));
         System.out.println(minInNight);
 
     }
@@ -29,7 +29,7 @@ public class TexiFee {
         int FUEL = 1;
 
         double totalMileage = 0;
-        int totalTime = 0; // 单位：分钟
+        Time totalTime = null;
         int lowSpeedTime = 0; // 单位：分钟
         int totalPrice = 0;
         int nightFee = 0;
@@ -38,10 +38,15 @@ public class TexiFee {
 
         System.out.println("请输入总里程数：");
         totalMileage = in.nextDouble();
-        System.out.println("请输入乘车总时间：（单位：分钟）");
-        totalTime = in.nextInt();
-        System.out.println("是否是夜间行驶：（Y/N）");
-        isNight = in.next().equalsIgnoreCase("Y");
+
+        System.out.println("请输入起步时间：（格式：00:00）");
+        String[] startTimeString = in.next().split(":");
+        Time startTime = new Time(Integer.parseInt(startTimeString[0]), Integer.parseInt(startTimeString[1]));
+        System.out.println("请输入下车时间：（格式：00:00）");
+        String[] endTimeString = in.next().split(":");
+        Time endTime = new Time(Integer.parseInt(endTimeString[0]), Integer.parseInt(endTimeString[1]));
+        int duringTime = ((endTime.toMinute() + 24 * 60) - startTime.toMinute()) % 24 * 60;
+        totalTime = new Time(duringTime / 60, duringTime % 60);
         System.out.println("是否预约叫车？（Y/N）");
         if (in.next().equalsIgnoreCase("Y")) {
             System.out.println("预约是否在4小时内？（Y/N）");
@@ -63,9 +68,6 @@ public class TexiFee {
             }
         }
 
-        if (isNight) {
-            nightFee = (int) (totalMileage * STANDARD_PRICE * 0.2);
-        }
 
         if (totalMileage > 15) {
             System.out.println("是否往返2公里范围内？（Y/N）");
@@ -76,6 +78,9 @@ public class TexiFee {
             }
         }
 
+        nightFee = (int) (inNight(startTime, endTime).toMinute() * (totalMileage - 1) * STANDARD_PRICE * 0.2);
+
+
         if (totalMileage < 3) {
             totalPrice = (int) (START_FEE + orderFee + emptyDriveFee + nightFee + FUEL);
         } else {
@@ -84,9 +89,13 @@ public class TexiFee {
 
         System.out.println("-----------invoice-----------");
         System.out.println("start fee: 13\n" +
+                "start time: " + startTime.toString() + "\n" +
+                "end time: " + endTime.toString() + "\n" +
+                "total duration time: " + totalTime.toString() + "\n" +
                 "total mileage: " + totalMileage + "\n" +
                 "order fee: " + orderFee + "\n" +
                 "low speed fee: " + lowSpeedFee + "\n" +
+                "night duration time:" + inNight(startTime, endTime).toString() + "\n" +
                 "night fee: " + nightFee + "\n" +
                 "empty drive fee: " + emptyDriveFee + "\n" +
                 "fuel surcharge: " + FUEL + "\n" +
@@ -97,23 +106,23 @@ public class TexiFee {
     /**
      * 根据开始时间和结束时间推算在早晚高峰内的时间
      *
-     * @param start 开始时间
-     * @param end   结束时间
+     * @param start   开始时间
+     * @param end     结束时间
      * @param morning 判断是否是早上的高峰/晚上的高峰
      * @return 处在高峰时间内的分钟数
      */
     public static int minutesInRushHour(Time start, Time end, boolean morning) {
         //早高峰时间段（分钟）：
-        int rushStart = morning?new Time(7, 0).toMinute():new Time(17,0).toMinute();
-        int rushEnd = morning?new Time(8, 59).toMinute():new Time(18,59).toMinute();
+        int rushStart = morning ? new Time(7, 0).toMinute() : new Time(17, 0).toMinute();
+        int rushEnd = morning ? new Time(8, 59).toMinute() : new Time(18, 59).toMinute();
         int rushDuration = 0;
-        if (start.toMinute()>=rushStart&&end.toMinute()<=rushEnd){
-            rushDuration = end.toMinute()-start.toMinute();
-        }else if(start.toMinute()<rushStart&&end.toMinute()>rushEnd){
+        if (start.toMinute() >= rushStart && end.toMinute() <= rushEnd) {
+            rushDuration = end.toMinute() - start.toMinute();
+        } else if (start.toMinute() < rushStart && end.toMinute() > rushEnd) {
             rushDuration = rushEnd - rushStart;
-        }else if(start.toMinute()<rushStart && end.toMinute()>rushStart&&end.toMinute()<rushEnd){
+        } else if (start.toMinute() < rushStart && end.toMinute() > rushStart && end.toMinute() < rushEnd) {
             rushDuration = end.toMinute() - rushStart;
-        }else if(start.toMinute()>rushStart&&start.toMinute()<rushEnd&&end.toMinute()>rushEnd){
+        } else if (start.toMinute() > rushStart && start.toMinute() < rushEnd && end.toMinute() > rushEnd) {
             rushDuration = rushEnd - start.toMinute();
         }
         return rushDuration;
@@ -158,32 +167,33 @@ public class TexiFee {
 
     /**
      * 夜间行车的时间（分钟）
+     *
      * @param start 开始时间
-     * @param end 结束时间
+     * @param end   结束时间
      * @return 在夜间行车的分钟数
      */
-    public static Time inNight(Time start, Time end){
+    public static Time inNight(Time start, Time end) {
         int minuteInNight = 0;
-        int nightStart = new Time(23,0).toMinute();
-        int nightEnd = new Time(4,59).toMinute();
-        int midNight = new Time(24,0).toMinute();
+        int nightStart = new Time(23, 0).toMinute();
+        int nightEnd = new Time(4, 59).toMinute();
+        int midNight = new Time(24, 0).toMinute();
         //起止时间在夜间范围内
-        if ((start.toMinute()>=nightStart && end.toMinute()<=nightEnd)||(start.toMinute()>=0&&end.toMinute()<=nightEnd) ||
-                (start.toMinute()>=nightStart && end.toMinute()<=midNight)){
+        if ((start.toMinute() >= nightStart && end.toMinute() <= nightEnd) || (start.toMinute() >= 0 && start.toMinute() < nightEnd && end.toMinute() <= nightEnd) ||
+                (start.toMinute() >= nightStart && end.toMinute() <= midNight)) {
             int subMin = end.toMinute() - start.toMinute();
-            minuteInNight = subMin<0?subMin+24*60:subMin;
-        }else if((start.toMinute()<=nightStart&&start.toMinute()>nightEnd) && (end.toMinute()>=nightEnd && end.toMinute()<nightStart)){
-        //起止时间在夜间范围外
-            minuteInNight = nightEnd-nightStart+24*60;
-        }else if((start.toMinute()<nightStart&&start.toMinute()>nightEnd) && (end.toMinute()<nightEnd || end.toMinute()<midNight)){
-        //起时间在夜间外，止时间在夜间内
+            minuteInNight = subMin < 0 ? subMin + 24 * 60 : subMin;
+        } else if ((start.toMinute() <= nightStart && start.toMinute() > nightEnd) && (end.toMinute() >= nightEnd && end.toMinute() < nightStart)) {
+            //起止时间在夜间范围外
+            minuteInNight = nightEnd - nightStart + 24 * 60;
+        } else if ((start.toMinute() < nightStart && start.toMinute() > nightEnd) && (end.toMinute() < nightEnd || end.toMinute() < midNight)) {
+            //起时间在夜间外，止时间在夜间内
             int subMin = end.toMinute() - nightStart;
-            minuteInNight = subMin<0?subMin+24*60:subMin;
-        }else if((end.toMinute()>=nightEnd && end.toMinute()<nightStart) && (start.toMinute()>nightStart || start.toMinute()<nightEnd)){
-        //起时间在夜间内，止时间在夜间外
-            int subMin = nightEnd-start.toMinute();
-            minuteInNight = subMin<0?subMin+24*60:subMin;
+            minuteInNight = subMin < 0 ? subMin + 24 * 60 : subMin;
+        } else if ((end.toMinute() >= nightEnd && end.toMinute() < nightStart) && (start.toMinute() > nightStart || start.toMinute() < nightEnd)) {
+            //起时间在夜间内，止时间在夜间外
+            int subMin = nightEnd - start.toMinute();
+            minuteInNight = subMin < 0 ? subMin + 24 * 60 : subMin;
         }
-        return new Time(minuteInNight/60, minuteInNight%60);
+        return new Time(minuteInNight / 60, minuteInNight % 60);
     }
 }
